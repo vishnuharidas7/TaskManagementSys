578235531fb7788621d16fec723bb550ab3b678c
gouthamr98 <goutham.r@nestgroup.net>
2025-07-29 10:38:29 +0530

    Added auth and LogController test

---
+++ TaskManagementWebAPITest/Controller/AuthControllerTest.cs
@@ -0,0 +1,119 @@
+﻿using LoggingLibrary.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementWebAPI.Application.DTOs;
+using TaskManagementWebAPI.Application.Interfaces;
+using TaskManagementWebAPI.Controllers;
+using TaskManagementWebAPI.Domain.Interfaces;
+using TaskManagementWebAPI.Domain.Models;
+
+namespace TaskManagementWebAPITest.Controller
+{
+    public class AuthControllerTest
+    {
+        private readonly AuthController _authController;
+        private readonly Mock<IUserAuthRepository> _userAuthRepoMock;
+        private readonly Mock<IAppLogger<AuthController>> _loggerMock;
+        private readonly Mock<IForgotPasswordHandler> _forgotPasswordHandlerMock;
+        public AuthControllerTest() {
+            _userAuthRepoMock = new Mock<IUserAuthRepository>();
+            _loggerMock = new Mock<IAppLogger<AuthController>>();
+            _forgotPasswordHandlerMock = new Mock<IForgotPasswordHandler>();
+            _authController = new AuthController(
+                
+               _userAuthRepoMock.Object,
+               _loggerMock.Object,
+               _forgotPasswordHandlerMock.Object);
+        }
+
+        [Fact]
+        public async Task ExternalLogin_ValidCredentials_ReturnsOkWithToken()
+        {
+            //Arrange
+            var dto = new LoginDTO
+            {
+                UserName = "amal",
+                Password = "amal"
+            };
+
+            var expectedToken = "sample-Token@123";
+
+            _userAuthRepoMock.Setup(repo=>repo.LoginAsync(dto)).ReturnsAsync(expectedToken);
+            
+            //Act
+            var result= await _authController.ExternalLogin(dto);
+            //Assert
+            var okResult=Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(expectedToken, okResult.Value);
+                
+        }
+        [Fact]
+        public async Task Refresh_ValidTokens_ReturnsOkWithNewToken()
+        {
+            //Arrange
+            var dto = new TokenResponseDTO
+            {
+                RefreshToken= "Old-Token-sample-Token@123"
+            };
+
+            var expectedNewToken = "New-Token-sample-Token@123";
+
+            _userAuthRepoMock.Setup(repo => repo.Refresh(dto)).ReturnsAsync(expectedNewToken);
+
+            //Act
+            var result = await _authController.Refresh(dto);
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(expectedNewToken, okResult.Value);
+
+        }
+
+        [Fact]
+        public async Task ForgotPassword_ReturnsOk_WhenUserIsFound()
+        {
+            // Arrange
+            var request = new ForgotPasswordRequest { Email = "test@gmailsample.com" };
+            var mockUser = new Users(); // or your actual user class
+
+            _forgotPasswordHandlerMock
+                .Setup(h => h.HandleAsync(request))
+                .ReturnsAsync(mockUser);
+
+            // Act
+            var result = await _authController.ForgotPassword(request);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var jObject = JObject.FromObject(okResult.Value);
+            Assert.Equal("New credentials has been sent to the provided Email.", jObject["Message"]);
+        }
+
+        [Fact]
+        public async Task ForgotPassword_ReturnsNotFound_WhenUserIsNull()
+        {
+            // Arrange
+            var request = new ForgotPasswordRequest { Email = "notfound@gmailsample.com" };
+
+            _forgotPasswordHandlerMock
+                .Setup(h => h.HandleAsync(request))
+                .ReturnsAsync((Users)null); // Simulate not found
+
+            // Act
+            var result = await _authController.ForgotPassword(request);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            var jObject = JObject.FromObject(notFoundResult.Value);
+            Assert.Equal("User with provided email does not exist.", jObject["Error"]?.ToString());
+        }
+
+    }
+}
---
+++ TaskManagementWebAPITest/Controller/LogControllerTest.cs
@@ -0,0 +1,137 @@
+﻿using LoggingLibrary.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementWebAPI.Application.DTOs;
+using TaskManagementWebAPI.Controllers;
+
+namespace TaskManagementWebAPITest.Controller
+{
+    public class LogControllerTest
+    {
+        private readonly Mock<IAppLogger<LogsController>> _loggerMock;
+        private readonly LogsController _logsController;
+
+        public LogControllerTest() {
+            _loggerMock= new Mock<IAppLogger<LogsController>>();
+            _logsController = new LogsController(
+                _loggerMock.Object
+                );
+
+        }
+        [Fact]
+        public void PostLog_ReturnsBadRequest_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            _logsController.ModelState.AddModelError("Message", "Required");
+            var logDto = new ClientLogDto();
+
+            // Act
+            var result = _logsController.postLog(logDto);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public void PostLog_ReturnsBadRequest_WhenLogDTONull()
+        {
+            // Arrange
+            var logDto = (ClientLogDto)null;
+
+            // Act
+            var result = _logsController.postLog(logDto);
+
+            // Assert
+            var badRequest= Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Log data is required", badRequest.Value);
+          
+        }
+        [Fact]
+        public void PostLog_ReturnsOk_ForValidInfoLog()
+        {
+            // Arrange
+            var logDto = new ClientLogDto
+            {
+                Level = "INFO",
+                Message = "This is an info log.",
+                Url = "/home",
+                Timestamp = DateTime.UtcNow.ToString(),
+                Error = null
+            };
+
+            // Act
+            var result = _logsController.postLog(logDto);
+
+            // Assert
+            var okResult = Assert.IsType<OkResult>(result);
+            _loggerMock.Verify(logger => logger.LoggInformation(It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public void PostLog_ReturnsOk_ForValidWarningLog()
+        {
+            // Arrange
+            var logDto = new ClientLogDto
+            {
+                Level = "WARNING",
+                Message = "This is a warning.",
+                Url = "/dashboard",
+                Timestamp = DateTime.UtcNow.ToString(),
+                Error = null
+            };
+
+            // Act
+            var result = _logsController.postLog(logDto);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+            _loggerMock.Verify(logger => logger.LoggWarning(It.IsAny<string>()), Times.Once);
+        }
+        [Fact]
+        public void PostLog_ReturnsOk_ForValidErrorLog()
+        {
+            // Arrange
+            var logDto = new ClientLogDto
+            {
+                Level = "ERROR",
+                Message = "Something went wrong.",
+                Url = "/error",
+                Timestamp = DateTime.UtcNow.ToString(),
+                Error = "NullReferenceException"
+            };
+
+            // Act
+            var result = _logsController.postLog(logDto);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+            _loggerMock.Verify(logger => logger.LoggError(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+        }
+        [Fact]
+        public void PostLog_LogsInfo_WhenLevelIsUnknown()
+        {
+            // Arrange
+            var logDto = new ClientLogDto
+            {
+                Level = "UNKNOWN",
+                Message = "Unknown level log.",
+                Url = "/unknown",
+                Timestamp = DateTime.UtcNow.ToString(),
+                Error = null
+            };
+
+            // Act
+            var result = _logsController.postLog(logDto);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+            _loggerMock.Verify(logger => logger.LoggInformation(It.IsAny<string>()), Times.Once);
+        }
+
+    }
+}
