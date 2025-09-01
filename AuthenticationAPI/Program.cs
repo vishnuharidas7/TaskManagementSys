79958e11dbb41fb893ac21cb80e7a5a13986cf39
vishnuharidas7 <vishnu.haridas@nestgroup.net>
2025-09-01 12:09:50 +0530

    updated the corspolicy

--- AuthenticationAPI/Program.cs
+++ AuthenticationAPI/Program.cs
@@ -113,8 +113,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
         policy => policy
-        .WithOrigins("http://localhost:4200")
-        //                .AllowAnyOrigin()
+       // .WithOrigins("http://localhost:4200")
+                        .AllowAnyOrigin()
                         .AllowAnyHeader()
                         .AllowAnyMethod());
 });
